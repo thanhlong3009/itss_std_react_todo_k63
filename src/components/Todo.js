9070af1b9f4d588b9dd6679d9a17bda74f345196
thanhlong3009 <thanhlongite6@gmail.com>
2022-06-06 10:45:42 +0700

    add local storage

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -19,27 +19,58 @@ import useStorage from '../hooks/storage';
 import {getKey} from "../lib/util";
 
 function Todo() {
-  const [items, putItems] = React.useState([
-      /* テストコード 開始 */
-    { key: getKey(), text: '日本語の宿題', done: false },
-    { key: getKey(), text: 'reactを勉強する', done: false },
-    { key: getKey(), text: '明日の準備をする', done: false },
-    /* テストコード 終了 */
-  ]);
-
+  const [items, putItems, clearItems] = useStorage();
+  //  const [items, putItems] = React.useState([
+  //     /* テストコード 開始 */
+  //   { key: getKey(), text: '日本語の宿題', done: false },
+  //   { key: getKey(), text: 'reactを勉強する', done: false },
+  //   { key: getKey(), text: '明日の準備をする', done: false },
+  //   /* テストコード 終了 */
+  // ]);
+  const [filter,setFilter] = useState('ALL');
+  const displayItems = items.filter(item => {
+    if (filter === 'ALL') return true;
+    if (filter === 'TODO') return !item.done;
+    if (filter === 'DONE') return item.done;
+  });
+ 
+  const handleFilterChange = value => setFilter(value);
+  const handleCheck = checked => {
+    const newItems = items.map(item => {
+      if (item.key === checked.key) {
+        item.done = !item.done;
+      }
+      return item;
+    });
+    putItems(newItems);
+  };
+  const handleAdd = text => {
+    putItems([...items, { key: getKey(), text, done: false }]);
+  };
   return (
     <div className="panel">
       <div className="panel-heading">
         ITSS ToDoアプリ
       </div>
-      {items.map(item => (
-        <label className="panel-block">
-            <input type="checkbox" />
-            {item.text}
-        </label>
+      {displayItems.map(item => (
+        <TodoItem
+          key={item.key}
+          item={item}
+          onCheck={handleCheck}
+        />
       ))}
       <div className="panel-block">
-        {items.length} items
+        {displayItems.length} items
+      </div>
+      <Input onAdd = {handleAdd}/>
+      <Filter
+        onChange={handleFilterChange}
+        value={filter}
+      />
+      <div className="panel-block">
+        <button style={{color: "red",backgroundColor: "white",opacity:0.5}} onClick={clearItems}>
+          すべてを削除する
+        </button>
       </div>
     </div>
   );
